e7ba56896c82afb7ecfdf18fe1ad976ebceaf482
Sanket Kogekar <ksanket1998@gmail.com>
2019-12-14 15:25:57 +0530

    Initial commit

---
+++ React-Methods.js
@@ -0,0 +1,33 @@
+const JSX = (
+  <div>
+    <h1>Hello World</h1>
+    <p>Lets render this to the DOM</p>
+  </div>
+);
+// change code below this line
+ReactDOM.render(JSX, document.getElementById("challenge-node"));
+
+//////
+
+
+const JSX = (
+  <div className="myDiv">
+    <h1>Add a class to this div</h1>
+  </div>
+);
+
+
+//////
+
+const JSX = (
+  <div>
+    <h2>Welcome to React!</h2> 
+    <br />
+    <p>Be sure to close all tags!</p>
+    <hr />
+  </div>
+);
+
+
+//////
+
