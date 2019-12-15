5bb4a44e529267a5558e58b6688b35dceb2c2150
Sanket Kogekar <ksanket1998@gmail.com>
2019-12-15 23:12:26 +0530

    2nd

--- React-Methods.js
+++ React-Methods.js
@@ -31,3 +31,63 @@ const JSX = (
 
 //////
 
+
+const MyComponent = function() {
+  // change code below this line
+  return(
+    <div>
+      AnyText
+    </div>
+  );
+  // change code above this line
+}
+
+/////
+
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    // change code below this line
+return(
+<div>
+<h1>Hello React!</h1>
+</div>
+);
+    // change code above this line
+  }
+};
+
+
+////////
+
+const ChildComponent = () => {
+  return (
+    <div>
+      <p>I am the child</p>
+    </div>
+  );
+};
+
+class ParentComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>I am the parent</h1>
+        { /* change code below this line */ }
+<ChildComponent/>
+
+        { /* change code above this line */ }
+      </div>
+    );
+  }
+};
+
+
+///////////
+
+
