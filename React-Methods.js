54723046a12e66abdbefa96874a22d5da736ff6f
Sanket Kogekar <ksanket1998@gmail.com>
2019-12-26 19:43:40 +0530

    using constructors

--- React-Methods.js
+++ React-Methods.js
@@ -361,3 +361,69 @@ Camper.propTypes = {
 };
 
 ////////
+
+class StatefulComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    // initialize state here
+this.state = {
+  // describe your state here
+  name : "Kogo"
+}
+  }
+  render() {
+    return (
+      <div>
+        <h1>{this.state.name}</h1>
+      </div>
+    );
+  }
+};
+
+
+///////
+
+
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'freeCodeCamp'
+    }
+  }
+  render() {
+    return (
+      <div>
+        { /* change code below this line */ }
+<h1>{this.state.name}</h1>
+        { /* change code above this line */ }
+      </div>
+    );
+  }
+};
+
+////
+
+
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'freeCodeCamp'
+    }
+  }
+  render() {
+    // change code below this line
+const name = this.state.name;
+    // change code above this line
+    return (
+      <div>
+        { /* change code below this line */ }
+          <h1>{name}</h1>
+        { /* change code above this line */ }
+      </div>
+    );
+  }
+};
+
+
