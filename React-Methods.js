d343196d2a0b60f96f2755385549958004d62e85
Sanket Kogekar <ksanket1998@gmail.com>
2019-12-26 12:45:38 +0530

    Stateless functional components

--- React-Methods.js
+++ React-Methods.js
@@ -295,3 +295,69 @@ class ShoppingCart extends React.Component {
 
 //////
 
+class ReturnTempPassword extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+  render() {
+    return (
+        <div>
+            { /* change code below this line */ }
+            <p>Your temporary password is: <strong>
+            {this.props.tempPassword}
+            </strong></p>
+            { /* change code above this line */ }
+        </div>
+    );
+  }
+};
+
+class ResetPassword extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+  render() {
+    return (
+        <div>
+          <h2>Reset Password</h2>
+          <h3>We've generated a new temporary password for you.</h3>
+          <h3>Please reset this password from your account settings ASAP.</h3>
+          { /* change code below this line */ }
+<ReturnTempPassword tempPassword="12341234" />
+          { /* change code above this line */ }
+        </div>
+    );
+  }
+};
+
+
+///////
+
+
+class CampSite extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <Camper/>
+      </div>
+    );
+  }
+};
+// change code below this line
+
+const Camper = props => <p>{props.name}</p>;
+
+Camper.defaultProps = {
+  name: "CamperBot"
+};
+
+Camper.propTypes = {
+  name: PropTypes.string.isRequired
+};
+
+////////
