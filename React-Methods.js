4ff096aa5939129d0c81624879b381b9c3ee5657
Sanket Kogekar <ksanket1998@gmail.com>
2019-12-28 12:20:05 +0530

    adding 'this'

--- React-Methods.js
+++ React-Methods.js
@@ -426,4 +426,70 @@ const name = this.state.name;
   }
 };
 
+///////////
+
+
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'Initial State'
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    // change code below this line
+this.setState({
+  name: 'React Rocks!'
+});
+    // change code above this line
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>Click Me</button>
+        <h1>{this.state.name}</h1>
+      </div>
+    );
+  }
+};
+
+
+///////
+
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: "Hello"
+    };
+    // change code below this line
+    this.handleClick = this.handleClick.bind(this);
+
+    // change code above this line
+  }
+  handleClick() {
+    this.setState({
+      text: "You clicked!"
+    });
+  }
+  render() {
+    return (
+      <div>
+        { /* change code below this line */ }
+        <button onClick = {this.handleClick}>Click Me</button>
+        { /* change code above this line */ }
+        <h1>{this.state.text}</h1>
+      </div>
+    );
+  }
+};
+
+///////
+
+
+
+
+
+
 
